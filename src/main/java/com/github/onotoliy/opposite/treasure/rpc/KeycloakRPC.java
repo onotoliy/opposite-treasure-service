e28aaa190d294065b8f7bf6acbbd0f925b54a2b4
Anatoliy Pokhresnyi <a.k.pohresniy@gmail.com>
2020-08-30 23:40:46 +0800

    notification

--- src/main/java/com/github/onotoliy/opposite/treasure/rpc/KeycloakRPC.java
+++ src/main/java/com/github/onotoliy/opposite/treasure/rpc/KeycloakRPC.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
