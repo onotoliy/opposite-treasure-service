fa63cf7d9feb2977686df864bfc9401446154c3a
Anatolij Pohresnyj <a.pokhresniy@playa.ru>
2021-01-09 14:24:37 +0800

    Добавленно API синхронизации.

--- src/main/java/com/github/onotoliy/opposite/treasure/web/UserResource.java
+++ src/main/java/com/github/onotoliy/opposite/treasure/web/UserResource.java
@@ -3,8 +3,11 @@ package com.github.onotoliy.opposite.treasure.web;
 import com.github.onotoliy.opposite.data.Option;
 import com.github.onotoliy.opposite.data.User;
 import com.github.onotoliy.opposite.treasure.rpc.KeycloakRPC;
+import com.github.onotoliy.opposite.treasure.services.NotificationService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,13 +30,21 @@ public class UserResource {
     private final KeycloakRPC user;
 
     /**
+     * Сервис уведомлений.
+     */
+    private final NotificationService notification;
+
+    /**
      * Конструктор.
      *
      * @param user Сервис чтения пользователей системы.
+     * @param notification Сервис уведомлений.
      */
     @Autowired
-    public UserResource(final KeycloakRPC user) {
+    public UserResource(final KeycloakRPC user,
+                        final NotificationService notification) {
         this.user = user;
+        this.notification = notification;
     }
 
     /**
@@ -77,5 +88,14 @@ public class UserResource {
     @GetMapping(value = "/list/full")
     public List<User> getFullDTOAll() {
         return user.getAll();
+    }
+
+    /**
+     * Отправка отчета о долгах.
+     */
+    @PostMapping(value = "/notification")
+    public void notification() {
+        notification.debts();
+        notification.deposit();
     }
 }
