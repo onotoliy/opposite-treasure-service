1c4d634c279e3cef682897b8be2f2e8034c02137
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2025-03-19 14:08:58 +0800

    SSO

--- src/main/java/com/github/onotoliy/opposite/treasure/SSOConfiguration.java
+++ src/main/java/com/github/onotoliy/opposite/treasure/SSOConfiguration.java
@@ -13,6 +13,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -51,19 +55,50 @@ public class SSOConfiguration {
     }
 
     @Bean
+    public AuthenticationProvider keycloakAuthenticationProvider() {
+        return new KeycloakAuthenticationProvider();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(keycloakAuthenticationProvider());
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration clientRegistration = ClientRegistration
+                .withRegistrationId("keycloak")
+                .clientId("myclient")
+                .clientSecret("myclientsecret")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)  // Указываем grant type
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email")
+                .authorizationUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/auth")
+                .tokenUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/token")
+                .userInfoUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/userinfo")
+                .jwkSetUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/certs")
+                .build();
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer. withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(registry -> {
             registry
                     .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/swagger-ui.html").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
                     .requestMatchers("/user/register/exception").permitAll()
                     .requestMatchers(HttpMethod.POST).hasAnyRole(modification)
                     .requestMatchers(HttpMethod.PUT).hasAnyRole(modification)
                     .requestMatchers(HttpMethod.DELETE).hasAnyRole(modification)
                     .requestMatchers(HttpMethod.GET).hasAnyRole(reading);
-        });
+        }).oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 }
