dfb3aa5786d7c19dcf97796499e2269d4c3cb55d
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2025-03-21 14:01:57 +0800

    SSO

--- src/main/java/com/github/onotoliy/opposite/treasure/SSOConfiguration.java
+++ src/main/java/com/github/onotoliy/opposite/treasure/SSOConfiguration.java
@@ -1,22 +1,13 @@
 package com.github.onotoliy.opposite.treasure;
 
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -26,7 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
-@KeycloakConfiguration
 public class SSOConfiguration {
 
     /**
@@ -55,36 +45,6 @@ public class SSOConfiguration {
     }
 
     @Bean
-    public AuthenticationProvider keycloakAuthenticationProvider() {
-        return new KeycloakAuthenticationProvider();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(keycloakAuthenticationProvider());
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = ClientRegistration
-                .withRegistrationId("keycloak")
-                .clientId("myclient")
-                .clientSecret("myclientsecret")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)  // Указываем grant type
-                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-                .scope("openid", "profile", "email")
-                .authorizationUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/auth")
-                .tokenUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/token")
-                .userInfoUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/userinfo")
-                .jwkSetUri("http://localhost:8080/realms/myrealm/protocol/openid-connect/certs")
-                .build();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer. withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
@@ -98,7 +58,7 @@ public class SSOConfiguration {
                     .requestMatchers(HttpMethod.PUT).hasAnyRole(modification)
                     .requestMatchers(HttpMethod.DELETE).hasAnyRole(modification)
                     .requestMatchers(HttpMethod.GET).hasAnyRole(reading);
-        }).oauth2Login(Customizer.withDefaults());
+        });
         return http.build();
     }
 }
